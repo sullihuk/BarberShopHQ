b137b7a1cca358b07c5c03e3a1f04ba753b67187
tdds <tdds@localhost.localdomain>
2021-03-29 05:55:58 +0700

    Add rake to gitigonre

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/contrib/all'
+require 'sinatra/activerecord'
+
+set :database, 'sqlite3:barbershop.db'
+
+class Client < ActiveRecord::Base
+
+
+
+
+end
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
